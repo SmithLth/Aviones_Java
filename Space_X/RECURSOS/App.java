a84560adb80691bfe10d1ca915328509a4a4b647
SmithLth <smithlazarte365@gmail.com>
2022-11-13 11:30:51 -0400

    nuevo

--- Space_X/RECURSOS/App.java
+++
@@ -1,8 +0,0 @@
-package RECURSOS;
-import processing.core.*;
-
-public class App {
-    public static void main(String[] args) {
-        PApplet.main(new String[] { Procesos.class.getName() });
-    }
-}
